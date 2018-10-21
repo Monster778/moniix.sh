2d711e61f601ed3df106fd67d1b46b0f4056e78f
Monster8080 <43908973+Monster778@users.noreply.github.com>
2018-10-21 18:23:04 +0300

    Add files via upload

---
+++ Monx.sh
@@ -0,0 +1,59 @@
+
+apt update
+apt upgrade
+apt-get update
+apt-get upgrade
+apt install php
+apt install python
+apt install python2
+apt install git
+apt install golang
+apt install host 
+apt install nano
+apt install havij 
+apt install hydra
+apt install wireshark 
+apt install cmatrix
+pkg install figlet
+pkg install wget
+pkg install wget -y
+pkg install python2 -y
+pkg install python2-dev -y
+apt install wireshark 
+pkg install cowsay
+pkg install toilet
+pkg install ruby
+pkg install help 
+gem install lolcat
+pkg install curl
+pkg install wgetrc 
+pkg install unzip
+pkg install openssh
+pkg install tor
+pkg install tar
+pkg install zip
+pkg install net-tools
+pkg install unrar
+pkg install clang
+pkg install w3m
+pkg install proot
+pip2 install wget
+pip2 install requests
+apt update
+apt upgrade
+apt install git
+apt install Python
+apt install Python2
+apt installe Python3
+apt install nano
+apt install Python pipeapt install ruby
+pip install bundle
+apt-get install gcc
+apt-get install Build- essential
+apt-get install perl
+apt-get install git
+apt-get install libany-uri-escape-perl
+apt-get install libhtmal-html5-entites-numbered-perl
+apt-get install lbhtmal-parser-perl
+apt-get install libwww-perl
+echo installed Done 👍
